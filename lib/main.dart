ed75f393033c1512d28aff67271ab83797517038
Magaretha14 <magaretha.wahyunda@gmail.com>
2023-11-22 15:33:15 +0700

    integration test for lihat_antrian_test.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:aplikasipendaftaranklinik/view/login.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -23,7 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: colorPrimary,
       ),
-      home: const Login(),
+      home: Login(),
     );
   }
 }
