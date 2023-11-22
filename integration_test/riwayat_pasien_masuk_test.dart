6b748454fe620a83ea3a899b5d07c9468b72a577
Magaretha14 <magaretha.wahyunda@gmail.com>
2023-11-22 20:54:43 +0700

    make integration testing for riwayay pasien masuk

---
+++ integration_test/riwayat_pasien_masuk_test.dart
@@ -0,0 +1,27 @@
+import 'package:aplikasipendaftaranklinik/view/admin/riwayat_pasien_masuk.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('Riwayat Pasien Masuk', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    //app.main();
+    await Firebase.initializeApp();
+
+    // Wait for the app to settle.
+    await tester.pumpWidget(const MaterialApp(
+      home: RiwayatPasienMasuk(),
+    ));
+    await tester.pumpAndSettle();
+    await Future.delayed(const Duration(seconds: 5));
+
+    expect(find.text('Nama Pasien'), findsWidgets);
+    expect(find.text('Masuk'), findsWidgets);
+    expect(find.text('Keluar'), findsWidgets);
+    await tester.pumpAndSettle();
+  });
+}
